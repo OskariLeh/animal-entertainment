9abbfe605f142ab5779df12c0cdc32977842f514
Oskari <oskari.m.lehtonen@student.lut.fi>
2024-05-10 16:16:42 +0300

    button demo

---
+++ server/public/javascripts/main.js
@@ -0,0 +1,10 @@
+const greenButton = document.getElementById("green")
+const redButton = document.getElementById("red")
+
+greenButton.addEventListener("click", () => {
+    console.log("GREEN CLICKED")
+})
+
+redButton.addEventListener("click", () => {
+    console.log("RED CLICKED")
+})
