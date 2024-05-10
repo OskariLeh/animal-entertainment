ada6d2744a28eea56aeaf7b9a4c6920031ef56a4
Oskari <oskari.m.lehtonen@student.lut.fi>
2024-05-10 16:24:35 +0300

    videoStream

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const videoStream = require('raspberrypi-node-camera-web-streamer');
+const videoStream = require('./videoStream');
 videoStream.acceptConnections(app, {
     width: 1280,
     height: 720,
